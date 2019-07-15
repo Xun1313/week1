0acb301af280b2f31ada3927be01485cc0cb4a67
Adam <wlo2641110@gmail.com>
2019-07-15 11:56:27 +0800

    7/15 2

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: '/week1/',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined,
+}
